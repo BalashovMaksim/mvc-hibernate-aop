08a31d5e3f992e19b62cb8576d327be988f802b7
Maksim Balashov <tracerb@inbox.ru>
2023-10-28 20:19:07 +0300

    Adding a project to github

---
+++ src/main/java/com/maksim/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,25 @@
+package com.maksim.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+    @Around("execution(* com.maksim.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws  Throwable{
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println("Begin if " + methodName);
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+
+        System.out.println("End of " + methodName);
+
+        return targetMethodResult;
+    }
+}
